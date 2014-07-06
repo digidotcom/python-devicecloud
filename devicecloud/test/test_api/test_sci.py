3ea7832f161218e3f3dd507c4ca56926bbdcbed2
Paul Osborne <paul.osborne@etherios.com>
2014-07-06 15:43:55 -0500

    PYTHONDC-1: Add MPL license header to all source files

--- devicecloud/test/test_api/test_sci.py
+++ devicecloud/test/test_api/test_sci.py
@@ -1,5 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2014 Etherios, Inc. All rights reserved.
+# Etherios, Inc. is a Division of Digi International.
+
 import unittest
-from devicecloud import DeviceCloud
 from devicecloud.api.sci import DeviceTarget
 from devicecloud.test.test_utilities import HttpTestBase
 import httpretty
