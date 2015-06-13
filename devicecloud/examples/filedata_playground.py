1b030e4dbd919eee26f608d4f192b2eef14e432d
Paul Osborne <Paul.Osborne@digi.com>
2015-06-13 01:04:02 -0500

    legal: update copyright to use Digi name instead of Etherios/Digi
    Signed-off-by: Paul Osborne <Paul.Osborne@digi.com>

--- devicecloud/examples/filedata_playground.py
+++ devicecloud/examples/filedata_playground.py
@@ -2,8 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright (c) 2014 Etherios, Inc. All rights reserved.
-# Etherios, Inc. is a Division of Digi International.
+# Copyright (c) 2015 Digi International, Inc.
 from getpass import getpass
 
 from devicecloud import DeviceCloud
